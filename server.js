78949ad8d820048edd6d3c0aac8b9752b4dc121b
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-19 00:44:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 
-import usuarioRoutes from './routes/usuarios.js';
-
 const app = express();
-app.use(cors());
+
+// ⚠️ CONFIGURA CORS PARA TU FRONTEND
+app.use(cors({
+  origin: 'https://eimtcms.eimt.uoc.edu', // permite ese dominio
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
+// Rutas
+import usuarioRoutes from './routes/usuarios.js';
 app.use('/api/usuarios', usuarioRoutes);
 
 const PORT = process.env.PORT || 3000;
