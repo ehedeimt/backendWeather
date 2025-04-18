fbb949f01c48a4926159e26d7868029af09c0cf3
Ehedei <122817828+ehedeimt@users.noreply.github.com>
2025-04-19 00:34:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+import express from 'express';
+import cors from 'cors';
 
+import usuarioRoutes from './routes/usuarios.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/usuarios', usuarioRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
+});
